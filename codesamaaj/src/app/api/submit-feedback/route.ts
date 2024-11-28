55081040faa126c4dbc05aad4392af6357f3d423
poswalsameer <poswalsameer2003@gmail.com>
2024-11-28 23:41:31 +0530

    changes in env

--- codesamaaj/src/app/api/submit-feedback/route.ts
+++ codesamaaj/src/app/api/submit-feedback/route.ts
@@ -29,7 +29,7 @@ export async function POST(req: NextRequest){
             type: process.env.NEXT_PUBLIC_GOOGLE_TYPE,
             project_id: process.env.NEXT_PUBLIC_GOOGLE_PROJECT_ID,
             private_key_id: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY_ID,
-            private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+            private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY!,
             client_email: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
             client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
             auth_uri: process.env.NEXT_PUBLIC_GOOGLE_AUTH_URI,
