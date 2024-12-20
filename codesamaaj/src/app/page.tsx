2b57f6a5af8d346557ab11994cf0ca867e2943cf
poswalsameer <poswalsameer2003@gmail.com>
2024-12-21 02:08:33 +0530

    added client-side code for payment gateway

--- codesamaaj/src/app/page.tsx
+++ codesamaaj/src/app/page.tsx
@@ -4,9 +4,77 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Card } from "@/components/ui/card";
+import sha256 from 'crypto-js/sha256';
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
 export default function Home() {
+
   const router = useRouter();
+
+  const initialisePayment = async () => {
+
+    console.log("Values inside env: ");
+    console.log("merchant id" ,process.env.NEXT_PUBLIC_MERCHANT_ID);
+    console.log("salt key" ,process.env.NEXT_PUBLIC_SALT_KEY);
+    console.log("salt index" ,process.env.NEXT_PUBLIC_SALT_INDEX);
+
+    const transactionId = "MT-"+uuidv4().toString().slice(-6);
+    const merchantUserId = "MUserId-"+uuidv4().toString().slice(-6);
+    // console.log("Transaction Id: ", transactionId);
+
+    const requestPayload = {
+      "merchantId": process.env.NEXT_PUBLIC_MERCHANT_ID,
+      "merchantTransactionId":transactionId,
+      "merchantUserId": merchantUserId,
+      "amount": 100,
+      "redirectUrl": `http://localhost:3000/api/paymentStatus/${transactionId}`,
+      "redirectMode": "POST",
+      "callbackUrl": `http://localhost:3000/api/paymentStatus/${transactionId}`,
+      "mobileNumber": "9999999999",
+      "paymentInstrument": {
+        "type": "PAY_PAGE"
+      }
+    }
+
+    const dataPayload = JSON.stringify(requestPayload);
+    // console.log("This is the dataPayload: ", dataPayload);
+
+    const payloadConvertedTo64 = Buffer.from(dataPayload).toString("base64");
+    // console.log(payloadConvertedTo64);
+
+    const fullURL = payloadConvertedTo64 + "/pg/v1/pay" + process.env.NEXT_PUBLIC_SALT_KEY;
+    const dataSha256 = sha256(fullURL);
+
+    const checkSum = dataSha256 + "###" + process.env.NEXT_PUBLIC_SALT_INDEX;
+    // console.log("Checksum is: ", checkSum);
+
+    const UAT_PAY_API_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
+
+    try {
+      const response = await axios.post(UAT_PAY_API_URL, {
+        request: payloadConvertedTo64,
+      }, {
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+          "X-VERIFY": checkSum,
+        }
+      });
+      console.log("Response:", response.data);
+    } catch (error: any) {
+      if (error.response) {
+        console.log("Error Response:", error.response.data);
+      } else {
+        console.log("Error:", error.message);
+      }
+    }
+
+    // if( response.data. ){
+    //   router.push
+    // }
+
+  }
 
   return (
     <>
@@ -80,7 +148,7 @@ export default function Home() {
         <div className="space-x-6 my-5 sm:my-10">
           <Button
             className=" w-32 bg-gray-900 text-white transition-all delay-75 ease-linear hover:-translate-y-1"
-            onClick={() => router.push("/payment-gateway")}
+            onClick={initialisePayment}
           >
             {/* Sign Up */}
             Get started
